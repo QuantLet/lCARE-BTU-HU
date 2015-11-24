763424b5b3e798c7f2560a817556f32357503000
xuxiu <xiu.xu@hu-berlin.de>
2015-11-24 17:32:42 +0100

    modify the align

--- LCARE_Adaptive_Estimation_Length_005/LCARE_Adaptive_Estimation_Length_005.m
+++ LCARE_Adaptive_Estimation_Length_005/LCARE_Adaptive_Estimation_Length_005.m
@@ -20,84 +20,112 @@ lcare_length_005_d_week = Smooth_mean(len_005, 20);
 figure
 subplot(2, 3, 1); 
 plot(lcare_DAX_005_c(11, :), '-b', 'LineWidth', 1.5); 
-title('DAX', 'FontName', 'Times New Roman', 'FontSize',16); 
+title('DAX', 'FontName', 'Times New Roman', 'FontSize', 16); 
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 16); 
 hold on; 
 plot(lcare_length_005_d_week(:, 1), '-r', 'LineWidth', 1.5); 
-ylabel('Length'); ylim([1, 180]); xlim([1, 2348]); 
-Year   = {'2006', '2010', '2014'}; hold on; 
-set(gca, 'xtick', [1  1045 2088]);
-Length = {'60', '120', '180'}; hold on; 
+ylabel('Length'); 
+ylim([1, 180]); 
+xlim([1, 2348]); 
+Year   = {'2006', '2010', '2014'}; 
+hold on; 
+set(gca, 'xtick', [1 1045 2088]);
+Length = {'60', '120', '180'}; 
+hold on; 
 set(gca, 'ytick', [60 120 180]);
 set(gca, 'xticklabel', Year); 
-set(gca, 'yticklabel', Length); set(gca,'xgrid','on')
+set(gca, 'yticklabel', Length); 
+set(gca, 'xgrid', 'on')
 
-subplot(2, 3, 2); plot(lcare_FTSE_005_c(11, :), '-b', 'LineWidth', 1.5); 
-title('FTSE 100', 'FontName', 'Times New Roman', 'FontSize',16); 
+subplot(2, 3, 2); 
+plot(lcare_FTSE_005_c(11, :), '-b', 'LineWidth', 1.5); 
+title('FTSE 100', 'FontName', 'Times New Roman', 'FontSize', 16); 
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 16);  
 hold on; 
 plot(lcare_length_005_d_week(:, 2), '-r', 'LineWidth', 1.5);
-ylabel(''); ylim([1, 180]); xlim([1, 2348]); 
-Year   = {'2006', '2010', '2014'}; hold on; 
-set(gca, 'xtick', [1  1045 2088]);
-Length = {'60', '120', '180'}; hold on; 
+ylim([1, 180]); 
+xlim([1, 2348]); 
+Year   = {'2006', '2010', '2014'}; 
+hold on; 
+set(gca, 'xtick', [1 1045 2088]);
+Length = {'60', '120', '180'}; 
+hold on; 
 set(gca, 'ytick', [60 120 180]);
 set(gca, 'xticklabel', Year); 
-set(gca, 'yticklabel', Length); set(gca,'xgrid','on')
+set(gca, 'yticklabel', Length); 
+set(gca, 'xgrid', 'on')
 
 subplot(2, 3, 3); 
 plot(lcare_SP_005_c(11, :), '-b', 'LineWidth', 1.5); 
 title('S&P 500');
-title('S&P 500', 'FontName', 'Times New Roman', 'FontSize',16); 
+title('S&P 500', 'FontName', 'Times New Roman', 'FontSize', 16); 
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 16);  
 hold on; 
 plot(lcare_length_005_d_week(:, 3), '-r', 'LineWidth', 1.5);
-ylabel(''); ylim([1, 180]); xlim([1, 2348]); 
-Year   = {'2006', '2010', '2014'}; hold on; 
-set(gca, 'xtick', [1  1045 2088]);
-Length = {'60', '120', '180'}; hold on; 
+ylim([1, 180]); 
+xlim([1, 2348]); 
+Year   = {'2006', '2010', '2014'}; 
+hold on; 
+set(gca, 'xtick', [1 1045 2088]);
+Length = {'60', '120', '180'}; 
+hold on; 
 set(gca, 'ytick', [60 120 180]);
 set(gca, 'xticklabel', Year); 
-set(gca, 'yticklabel', Length); set(gca,'xgrid','on')
+set(gca, 'yticklabel', Length); 
+set(gca, 'xgrid', 'on')
 
 subplot(2, 3, 4); 
 plot(lcare_DAX_005_d(11, :), '-b', 'LineWidth', 1.5);
-title('DAX', 'FontName', 'Times New Roman', 'FontSize',16); 
+title('DAX', 'FontName', 'Times New Roman', 'FontSize', 16); 
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 16);  
 hold on; 
 plot(lcare_length_005_d_week(:, 4), '-r', 'LineWidth', 1.5);
-ylabel('Length'); ylim([1, 180]); xlim([1, 2348]); 
-Year   = {'2006', '2010', '2014'}; hold on; 
-set(gca, 'xtick', [1  1045 2088]);
-Length = {'60', '120', '180'}; hold on; 
+ylabel('Length'); 
+ylim([1, 180]); 
+xlim([1, 2348]); 
+Year   = {'2006', '2010', '2014'}; 
+hold on; 
+set(gca, 'xtick', [1 1045 2088]);
+Length = {'60', '120', '180'}; 
+hold on; 
 set(gca, 'ytick', [60 120 180]);
 set(gca, 'xticklabel', Year); 
-set(gca, 'yticklabel', Length); set(gca,'xgrid','on')
+set(gca, 'yticklabel', Length); 
+set(gca, 'xgrid', 'on')
 
 subplot(2, 3, 5); 
 plot(lcare_FTSE_005_d(11, :), '-b', 'LineWidth', 1.5);
-title('FTSE 100', 'FontName', 'Times New Roman', 'FontSize',16); 
+title('FTSE 100', 'FontName', 'Times New Roman', 'FontSize', 16); 
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 16);  
 hold on; 
 plot(lcare_length_005_d_week(:, 5), '-r', 'LineWidth', 1.5);
-ylabel(''); ylim([1, 180]); xlim([1, 2348]); 
-Year   = {'2006', '2010', '2014'}; hold on; 
-set(gca, 'xtick', [1  1045 2088]);
-Length = {'60', '120', '180'}; hold on; 
+ylim([1, 180]); 
+xlim([1, 2348]); 
+Year   = {'2006', '2010', '2014'}; 
+hold on; 
+set(gca, 'xtick', [1 1045 2088]);
+Length = {'60', '120', '180'}; 
+hold on; 
 set(gca, 'ytick', [60 120 180]);
 set(gca, 'xticklabel', Year); 
-set(gca, 'yticklabel', Length); set(gca,'xgrid','on')
+set(gca, 'yticklabel', Length); 
+set(gca, 'xgrid', 'on')
 
 subplot(2, 3, 6); 
 plot(lcare_SP_005_d(11, :), '-b', 'LineWidth', 1.5);
-title('S&P 500', 'FontName', 'Times New Roman', 'FontSize',16); 
+title('S&P 500', 'FontName', 'Times New Roman', 'FontSize', 16); 
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 16);  
-hold on; plot(lcare_length_005_d_week(:, 6), '-r', 'LineWidth', 1.5);
-ylabel(''); ylim([1, 180]); xlim([1, 2348]); 
-Year   = {'2006', '2010', '2014'}; hold on; 
-set(gca, 'xtick', [1  1045 2088]);
-Length = {'60', '120', '180'}; hold on; 
+hold on; 
+plot(lcare_length_005_d_week(:, 6), '-r', 'LineWidth', 1.5);
+ylim([1, 180]); 
+xlim([1, 2348]); 
+Year   = {'2006', '2010', '2014'}; 
+hold on; 
+set(gca, 'xtick', [1 1045 2088]);
+Length = {'60', '120', '180'}; 
+hold on; 
 set(gca, 'ytick', [60 120 180]);
 set(gca, 'xticklabel', Year); 
-set(gca, 'yticklabel', Length); set(gca,'xgrid','on')
+set(gca, 'yticklabel', Length); 
+set(gca, 'xgrid', 'on')
 
